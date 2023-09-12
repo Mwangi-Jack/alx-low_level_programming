66b7d0018f31c772299a606ad54322bddfb0ef11
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-09-12 15:54:19 +0300

    added multiple files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This program prints _putchar to the console
+ * Return: Always Success (0)
+ *
+*/
+
+int main(void)
+{
+putchar("putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - function
+ * Description: This program prints _putchar to the console
+ * Return: Always Success (0)
+ *
+*/
+
+void print_alphabet(void)
+{
+char i;
+for (i = "a"; i < "z"; i++)
+{
+putchar(i);
+}
+
+}
