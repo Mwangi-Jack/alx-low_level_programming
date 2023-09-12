9b46a8bb30a998c7002879227f68cd1a641de5ad
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-09-12 16:34:30 +0300

    added main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,33 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
-void print_alphabet(void);
+
+/**
+ * _putchar -writes the character c to std
+ * @c: the parameter to compare
+ * Return: either 1 or 0
+ */
+int _putchar(char c);
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: Always 0 (success)
+ */
 void print_alphabet_x10(void);
-int _isLower(int c);
+
+/**
+ * _islower - Print if argument is lowercase
+ * @c: argument to check
+ * Return: either 1 or 0
+ */
+int _islower(int c);
+
+/**
+ * _isalpha - Print if argument is alphabet
+ * @c: argument to check
+ * Return: Either 1 or 0
+ */
 int _isalpha(int c);
+
 #endif
