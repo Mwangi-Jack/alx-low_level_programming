ae575c7c8f1bed141d65f1ef7098effcaf67fbc2
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-09-18 23:31:28 +0300

    prints a string followed by a new line

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,29 +1,36 @@
 #include "main.h"
+
 /**
- * rev_string - prints reversed string, followed by a new line
- * @s: pointer to the string to print
- * Return: void
-*/
+ * _atoi - Convert a string to integer.
+ * @s: char array string
+ * Return: first integer found in string
+ */
 
-void rev_string(char *s)
+int _atoi(char *s)
 {
+	int i;
+	int h, p;
 
-int len, i, half;
-char temp;
+	h = 0;
+	p = -1;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			p *= -1;
 
-for (len = 0; s[len] != '\0'; len++)
-;
+		if (s[i] > 47 && s[i] < 58)
+		{
+			if (h < 0)
+				h = (h * 10) - (s[i] - '0');
+			else
+				h = (s[i] - '0') * -1;
 
-i = 0;
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
+	}
+	if (p < 0)
+		h *= -1;
 
-half = len / 2;
-
-while (half--)
-{
-	temp = s[len - i - 1];
-	s[len - i - 1] = s[i];
-	s[i] = temp;
-	i++;
-}
-
+	return (h);
 }
