3cdf1b53f6f1d68ccaf51b686d2beface17da6a0
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-09-18 23:24:54 +0300

    added putchar

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -121,4 +121,15 @@ char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
 
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
 #endif
