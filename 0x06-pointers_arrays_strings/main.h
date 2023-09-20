34df206c887bb03f1b87180689c463a76de8a99e
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-09-20 15:52:20 +0300

    function to compare strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,4 +14,40 @@
 
 char *_strcat(char *dest, char *src);
 
+
+/**
+ * _strncat - concats two arrays
+ *
+ * @dest: destination of concat
+ * @src: source array to concat
+ * @n: amount of times to append
+ *
+ * Return: char value
+ */
+
+char *_strncat(char *dest, char *src, int n);
+
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination to copy
+ * @src: source to copy
+ * @n: amount of places to copy
+ * Return: value of dest
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+
+
+/**
+ * _strcmp - string to compare
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: value of string
+ */
+
+int _strcmp(char *s1, char *s2);
+
+
 #endif
