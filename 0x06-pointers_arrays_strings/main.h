1228cba91395bc88325fe11c91f03f56d6e642f2
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-09-20 16:11:13 +0300

    function to print a buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -95,4 +95,16 @@ char *leet(char *s);
 
 void print_number(int n);
 
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size
+ * Return: the pointer to dest.
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 #endif
