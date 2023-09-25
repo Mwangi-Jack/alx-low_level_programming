2ddc88c400a9bd5f8b947865e38709ef6d024f0f
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-09-25 23:14:57 +0300

    added multiple files

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -17,13 +17,13 @@ char *_memset(char *s, char b, unsigned int n);
 
 /**
  * _memcpy - function
- * 
+ *
  * Description: This function copies memory area
- * 
+ *
  * @dest: this is where the memory area is copied to
  * @src: this is where the memory area is copied from
  * @n: this variable holds the number of bytes to copy
- * 
+ *
  * Return: the function returns a pointer to @dest
 */
 
@@ -45,12 +45,12 @@ char *_strchr(char *s, char c);
 
 /**
  * _strspn - function
- * 
+ *
  * Description: this functioin gets the length of a prefix substring
- * 
+ *
  * @s: string
  * @accept: string
- * 
+ *
  * Return: returns the number of bytes in the initial segment of @s
 */
 
@@ -58,15 +58,44 @@ unsigned int _strspn(char *s, char *accept);
 
 /**
  * _strpbrk- function
- * 
+ *
  * Description: this function searches a string for any of a set of bytes
- * 
+ *
  * @s: pointer of type char
  * @: pointer of type char
- * 
+ *
  * Return: returns a pointer to the byte in @s
 */
 
 char *_strpbrk(char *s, char *accept);
+
+
+/**
+ * _strstr - locates substring
+ * @haystack: string to locate
+ * @needle: substring to locate
+ * Return: returns string at starting sub or null
+ */
+
+char *_strstr(char *haystack, char *needle);
+
+
+/**
+ * *print_chessboard - description
+ * @a: board
+ * Return: void
+ */
+
+
+void print_chessboard(char (*a)[8]);
+
+
+/**
+ * print_diagsums - prints diagonal summaries
+ * @a: array to use
+ * @size: size of diagonal
+ */
+
+void print_diagsums(int *a, int size);
 
 #endif
