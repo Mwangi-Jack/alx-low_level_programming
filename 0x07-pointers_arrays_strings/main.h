725a9c8df2940a654e9f5ad3ab68fdabc0f89206
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-09-25 16:38:36 +0300

    memset func

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+/**
+ * _memset - function
+ *
+ * Description: This function fills memory with a constant byte
+ *
+ * @s:
+ * @b:
+ * @n:
+ * Return: Returns a pointer to the memory area s
+*/
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
