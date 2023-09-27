21a9daa0222a32c8e6350f657fef8aa6754264cd
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-09-27 16:34:44 +0300

    print string in reverse recursively

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -12,16 +12,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	int len;
-
 	if (*s)
 	{
-		len = strlen(s);
-		_putchar(s[len]);
-		_print_rev_recursion(++s);
-		len = len - 1;
-	} else
-	{
-		_putchar('\n');
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
