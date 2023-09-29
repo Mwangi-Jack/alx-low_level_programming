9977f9839ce88d02b0fbb75a65f9fbbdcd170855
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-09-29 16:28:08 +0300

    function to print first arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program to print its name
+ *
+ * @argv: an array of arguments
+ *
+ * @argc: the length of the array
+ *
+ * Return: Always Success (0)
+*/
+
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
