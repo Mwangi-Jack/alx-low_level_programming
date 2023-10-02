2feb13f5edca068de757f20f230e7f898f6c5192
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-10-02 10:40:04 +0300

    function to print arg count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: program to print the number of arguments passed to it
+ *
+ * @argv: hello world
+ * @argc: hello world
+ * Return: Always Success (0)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
