27e0b74a29d572db8af4d926840311e6932e0bbe
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-10-20 16:18:51 +0300

    add node at the beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,19 +13,15 @@
  * Return: returns the length
 */
 
-int _strlen(char *s)
+int _strlen(const char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (*(s + i) != '\0')
 		i++;
 
 	return (i);
 }
-
-
-
-
 
 
 /**
